8fa5887ea6f6bdc099efe6b503a0f145f7ca991a
Mojdeh Pajang <mojde.pajang@gmail.com>
2025-07-01 13:19:58 +0200

    feat(auth): add grouped routes for register and login

---
+++ apps/web/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Login() {
+  return (
+    <div>
+      <main>
+        <h1>Login</h1>
+      </main>
+    </div>
+  );
+}
---
+++ apps/web/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Register() {
+  return (
+    <div>
+      <main>
+        <h1>Register</h1>
+      </main>
+    </div>
+  );
+}
