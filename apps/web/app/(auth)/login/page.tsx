49df36461fd2b54ce9943ec2bc97d10c83d80d7d
Mojdeh Pajang <mojde.pajang@gmail.com>
2025-07-01 16:45:29 +0200

    feat(web/auth): add login page

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { LoginForm } from "@/components/login-form";
+
 export default function Login() {
   return (
     <div>
       <main>
-        <h1>Login</h1>
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+          <div className="w-full max-w-sm">
+            <LoginForm />
+          </div>
+        </div>
       </main>
     </div>
   );
