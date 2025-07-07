a86c37aaa272dc47b55492ff5f76bd7d2863a855
Mojdeh Pajang <mojde.pajang@gmail.com>
2025-07-07 22:47:04 +0200

    fix(web): refine field and form elements

--- packages/ui/src/components/field.tsx
+++ packages/ui/src/components/field.tsx
@@ -4,22 +4,18 @@ import { cn } from "@stroom/ui/lib/utils";
 
 type FieldPrimitivesRootProps = ComponentProps<typeof FieldPrimitives.Root>;
 type FieldPrimitivesLabelProps = ComponentProps<typeof FieldPrimitives.Label>;
-type FieldPrimitivesControlProps = ComponentProps<
-  typeof FieldPrimitives.Control
->;
 type FieldPrimitivesDescriptionProps = ComponentProps<
   typeof FieldPrimitives.Description
 >;
 type FieldPrimitivesErrorProps = ComponentProps<typeof FieldPrimitives.Error>;
-type FieldPrimitivesValidityProps = ComponentProps<
-  typeof FieldPrimitives.Validity
->;
 
-function FieldRoot(props: FieldPrimitivesRootProps) {
-  return <FieldPrimitives.Root {...props} />;
+function Field({ className, ...props }: FieldPrimitivesRootProps) {
+  return (
+    <FieldPrimitives.Root className={cn("grid gap-2", className)} {...props} />
+  );
 }
 
-function FieldLabel({ className, ...props }: FieldPrimitivesLabelProps) {
+function Label({ className, ...props }: FieldPrimitivesLabelProps) {
   return (
     <FieldPrimitives.Label
       className={cn("data-[error=true]:text-destructive", className)}
@@ -28,24 +24,9 @@ function FieldLabel({ className, ...props }: FieldPrimitivesLabelProps) {
   );
 }
 
-function FieldControl({ className, ...props }: FieldPrimitivesControlProps) {
-  return (
-    <FieldPrimitives.Control
-      {...props}
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input shadow-xs flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:ring-ring/50 focus-visible:border-ring focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive data-[focused]:bg-red-400",
-        className,
-      )}
-    />
-  );
-}
+const Control = FieldPrimitives.Control;
 
-function FieldDescription({
-  className,
-  ...props
-}: FieldPrimitivesDescriptionProps) {
+function Description({ className, ...props }: FieldPrimitivesDescriptionProps) {
   return (
     <FieldPrimitives.Description
       className={cn("text-muted-foreground text-sm", className)}
@@ -54,7 +35,7 @@ function FieldDescription({
   );
 }
 
-function FieldError({ className, ...props }: FieldPrimitivesErrorProps) {
+function Error({ className, ...props }: FieldPrimitivesErrorProps) {
   return (
     <FieldPrimitives.Error
       className={cn("text-destructive text-sm", className)}
@@ -63,15 +44,6 @@ function FieldError({ className, ...props }: FieldPrimitivesErrorProps) {
   );
 }
 
-function FieldValidity(props: FieldPrimitivesValidityProps) {
-  return <FieldPrimitives.Validity {...props} />;
-}
+const Validity = FieldPrimitives.Validity;
 
-export {
-  FieldRoot,
-  FieldLabel,
-  FieldControl,
-  FieldDescription,
-  FieldError,
-  FieldValidity,
-};
+export { Field, Label, Control, Description, Error, Validity };
--- packages/ui/src/components/form.tsx
+++ packages/ui/src/components/form.tsx
@@ -1,10 +1,3 @@
 import { Form as FormBaseUI } from "@base-ui-components/react/form";
-import { ComponentProps } from "react";
 
-type FormType = ComponentProps<typeof FormBaseUI>;
-
-function Form(props: FormType) {
-  return <FormBaseUI {...props} />;
-}
-
-export { Form };
+export { FormBaseUI as Form };
